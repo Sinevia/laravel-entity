175b28de64260455760fe4975881074da6b91600
Sinevia <info@sinevia.com>
2018-02-03 07:14:23 +0000

    Create 2018_02_03_000001_package_sinevia_object_tables_create.php

---
+++ database/migrations/2018_02_03_000001_package_sinevia_object_tables_create.php
@@ -0,0 +1,23 @@
+class PackageSineviaCmsTablesCreate extends Illuminate\Database\Migrations\Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Sinevia\Object\Models\Object::tableCreate();
+        Sinevia\Object\Models\Field::tableCreate();
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Sinevia\Object\Models\Object::tableDelete();
+        Sinevia\Object\Models\Field::tableDelete();
+    }
+
+}
