61218565e03a159af99889eb2fbca534a85a74a3
Sinevia <info@sinevia.com>
2018-05-08 03:53:53 +0100

    Update 2018_02_03_000001_package_sinevia_object_tables_create.php

--- database/migrations/2018_02_03_000001_package_sinevia_object_tables_create.php
+++ database/migrations/2018_02_03_000001_package_sinevia_object_tables_create.php
@@ -1,4 +1,4 @@
-class PackageSineviaCmsTablesCreate extends Illuminate\Database\Migrations\Migration {
+class PackageSineviaObjectTablesCreate extends Illuminate\Database\Migrations\Migration {
 
     /**
      * Run the migrations.
